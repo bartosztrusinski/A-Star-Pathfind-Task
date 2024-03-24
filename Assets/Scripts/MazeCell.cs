416483f21c6e3f94b24b8cac0b46691774d75cfc
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-03-24 18:41:20 +0100

    implement A* path finding

--- Assets/Scripts/MazeCell.cs
+++ Assets/Scripts/MazeCell.cs
@@ -1,48 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class MazeCell : MonoBehaviour
-{
-  public enum Side
-  {
-    Left,
-    Right,
-    Front,
-    Back
-  }
+// public class MazeCell : MonoBehaviour
+// {
+//   public enum Side
+//   {
+//     Left,
+//     Right,
+//     Front,
+//     Back
+//   }
 
-  public GameObject leftWall;
-  public GameObject rightWall;
-  public GameObject frontWall;
-  public GameObject backWall;
-  public GameObject unvisitedBlock;
+//   public GameObject leftWall;
+//   public GameObject rightWall;
+//   public GameObject frontWall;
+//   public GameObject backWall;
+//   public GameObject unvisitedBlock;
 
-  public bool IsVisited { get; private set; }
+//   public bool IsVisited { get; private set; }
 
-  public void Visit()
-  {
-    IsVisited = true;
-    unvisitedBlock.SetActive(false);
-  }
+//   public void Visit()
+//   {
+//     IsVisited = true;
+//     unvisitedBlock.SetActive(false);
+//   }
 
-  public void RemoveWall(Side side)
-  {
-    switch (side)
-    {
-      case Side.Left:
-        leftWall.SetActive(false);
-        break;
-      case Side.Right:
-        rightWall.SetActive(false);
-        break;
-      case Side.Front:
-        frontWall.SetActive(false);
-        break;
-      case Side.Back:
-        backWall.SetActive(false);
-        break;
-    }
-  }
+//   public void RemoveWall(Side side)
+//   {
+//     switch (side)
+//     {
+//       case Side.Left:
+//         leftWall.SetActive(false);
+//         break;
+//       case Side.Right:
+//         rightWall.SetActive(false);
+//         break;
+//       case Side.Front:
+//         frontWall.SetActive(false);
+//         break;
+//       case Side.Back:
+//         backWall.SetActive(false);
+//         break;
+//     }
+//   }
 
-}
+// }
--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -1,107 +1,107 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.Linq;
+// using UnityEngine;
 
-public class MazeGenerator : MonoBehaviour
-{
-  public MazeCell mazeCellPrefab;
-  public int mazeWidth;
-  public int mazeDepth;
+// public class MazeGenerator : MonoBehaviour
+// {
+//   public MazeCell mazeCellPrefab;
+//   public int mazeWidth;
+//   public int mazeDepth;
 
-  private MazeCell[,] mazeGrid;
+//   public MazeCell[,] mazeGrid;
 
-  void Start()
-  {
-    mazeGrid = new MazeCell[mazeWidth, mazeDepth];
+//   void Start()
+//   {
+//     mazeGrid = new MazeCell[mazeWidth, mazeDepth];
 
-    FillMaze();
+//     FillMaze();
 
-    GenerateMaze(null, mazeGrid[0, 0]); 
-  }
+//     GenerateMaze(null, mazeGrid[0, 0]); 
+//   }
 
-  private void FillMaze()
-  {
-    for (int x = 0; x < mazeWidth; x++)
-    {
-      for (int z = 0; z < mazeDepth; z++)
-      {
-        mazeGrid[x, z] = Instantiate(mazeCellPrefab, new Vector3(x, 0, z), Quaternion.identity);
-      }
-    }
-  }
+//   private void FillMaze()
+//   {
+//     for (int x = 0; x < mazeWidth; x++)
+//     {
+//       for (int z = 0; z < mazeDepth; z++)
+//       {
+//         mazeGrid[x, z] = Instantiate(mazeCellPrefab, new Vector3(x, 0, z), Quaternion.identity);
+//       }
+//     }
+//   }
 
-  private void GenerateMaze(MazeCell previousCell, MazeCell currentCell)
-  {
-    currentCell.Visit();
-    KnockDownWalls(previousCell, currentCell);
+//   private void GenerateMaze(MazeCell previousCell, MazeCell currentCell)
+//   {
+//     currentCell.Visit();
+//     KnockDownWalls(previousCell, currentCell);
     
-    MazeCell nextCell;
+//     MazeCell nextCell;
 
-    do {
-      nextCell = GetNextUnvisitedCell(currentCell);
+//     do {
+//       nextCell = GetNextUnvisitedCell(currentCell);
 
-      if (nextCell) GenerateMaze(currentCell, nextCell);
-    } while (nextCell);
-  }
+//       if (nextCell) GenerateMaze(currentCell, nextCell);
+//     } while (nextCell);
+//   }
 
-  private MazeCell GetNextUnvisitedCell(MazeCell currentCell)
-  {
-    var unvisitedCells = GetUnvisitedCells(currentCell).ToList();
+//   private MazeCell GetNextUnvisitedCell(MazeCell currentCell)
+//   {
+//     var unvisitedCells = GetUnvisitedCells(currentCell).ToList();
 
-    return unvisitedCells.OrderBy(_ => Random.Range(1, 10)).FirstOrDefault();
-  }
+//     return unvisitedCells.OrderBy(_ => Random.Range(1, 10)).FirstOrDefault();
+//   }
 
-  private IEnumerable<MazeCell> GetUnvisitedCells(MazeCell currentCell)
-  {
-    int x = (int)currentCell.transform.position.x;
-    int z = (int)currentCell.transform.position.z;
-    MazeCell leftCell = x > 0 ? mazeGrid[x - 1, z] : null;
-    MazeCell rightCell = x < mazeWidth - 1 ? mazeGrid[x + 1, z] : null;
-    MazeCell frontCell = z > 0 ? mazeGrid[x, z - 1] : null;
-    MazeCell backCell = z < mazeDepth - 1 ? mazeGrid[x, z + 1] : null;
+//   private IEnumerable<MazeCell> GetUnvisitedCells(MazeCell currentCell)
+//   {
+//     int x = (int)currentCell.transform.position.x;
+//     int z = (int)currentCell.transform.position.z;
+//     MazeCell leftCell = x > 0 ? mazeGrid[x - 1, z] : null;
+//     MazeCell rightCell = x < mazeWidth - 1 ? mazeGrid[x + 1, z] : null;
+//     MazeCell frontCell = z > 0 ? mazeGrid[x, z - 1] : null;
+//     MazeCell backCell = z < mazeDepth - 1 ? mazeGrid[x, z + 1] : null;
 
-    if(leftCell && !leftCell.IsVisited) yield return leftCell;
-    if(rightCell && !rightCell.IsVisited) yield return rightCell;
-    if(frontCell && !frontCell.IsVisited) yield return frontCell;
-    if(backCell && !backCell.IsVisited) yield return backCell;
-  }
+//     if(leftCell && !leftCell.IsVisited) yield return leftCell;
+//     if(rightCell && !rightCell.IsVisited) yield return rightCell;
+//     if(frontCell && !frontCell.IsVisited) yield return frontCell;
+//     if(backCell && !backCell.IsVisited) yield return backCell;
+//   }
 
-  private void KnockDownWalls(MazeCell previousCell, MazeCell currentCell)
-  {
-    if (!previousCell) return;
+//   private void KnockDownWalls(MazeCell previousCell, MazeCell currentCell)
+//   {
+//     if (!previousCell) return;
 
-    bool isPreviousCellLeft = previousCell.transform.position.x < currentCell.transform.position.x;
-    bool isPreviousCellRight = previousCell.transform.position.x > currentCell.transform.position.x;
-    bool isPreviousCellFront = previousCell.transform.position.z > currentCell.transform.position.z;
-    bool isPreviousCellBack = previousCell.transform.position.z < currentCell.transform.position.z;
+//     bool isPreviousCellLeft = previousCell.transform.position.x < currentCell.transform.position.x;
+//     bool isPreviousCellRight = previousCell.transform.position.x > currentCell.transform.position.x;
+//     bool isPreviousCellFront = previousCell.transform.position.z > currentCell.transform.position.z;
+//     bool isPreviousCellBack = previousCell.transform.position.z < currentCell.transform.position.z;
 
-    if (isPreviousCellLeft)
-    {
-      previousCell.RemoveWall(MazeCell.Side.Right);
-      currentCell.RemoveWall(MazeCell.Side.Left);
-      return;
-    }
+//     if (isPreviousCellLeft)
+//     {
+//       previousCell.RemoveWall(MazeCell.Side.Right);
+//       currentCell.RemoveWall(MazeCell.Side.Left);
+//       return;
+//     }
     
-    if (isPreviousCellRight)
-    {
-      previousCell.RemoveWall(MazeCell.Side.Left);
-      currentCell.RemoveWall(MazeCell.Side.Right);
-      return;
-    }
+//     if (isPreviousCellRight)
+//     {
+//       previousCell.RemoveWall(MazeCell.Side.Left);
+//       currentCell.RemoveWall(MazeCell.Side.Right);
+//       return;
+//     }
     
-    if (isPreviousCellFront)
-    {
-      previousCell.RemoveWall(MazeCell.Side.Back);
-      currentCell.RemoveWall(MazeCell.Side.Front);
-      return;
-    }
+//     if (isPreviousCellFront)
+//     {
+//       previousCell.RemoveWall(MazeCell.Side.Back);
+//       currentCell.RemoveWall(MazeCell.Side.Front);
+//       return;
+//     }
     
-    if (isPreviousCellBack)
-    {
-      previousCell.RemoveWall(MazeCell.Side.Front);
-      currentCell.RemoveWall(MazeCell.Side.Back);
-      return;
-    }
-  }
-}
+//     if (isPreviousCellBack)
+//     {
+//       previousCell.RemoveWall(MazeCell.Side.Front);
+//       currentCell.RemoveWall(MazeCell.Side.Back);
+//       return;
+//     }
+//   }
+// }
